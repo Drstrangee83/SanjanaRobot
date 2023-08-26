313388b8f788784e0620b0c5a5c9282f557f7718
Drstrangee83 <122635577+Drstrangee83@users.noreply.github.com>
2023-08-26 14:36:24 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -7,7 +7,7 @@ class Config(object):
     API_ID = "" # integer value, dont use ""
     API_HASH = ""
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 2145093972 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 6032136577 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
     SUPPORT_CHAT = "the_support_chat"  # Your own group for support, do not add the @
     START_IMG = ""
@@ -23,12 +23,12 @@ class Config(object):
 
     # Optional fields
     CHATBOT_API="" # get it from @FallenChat_Bot using /token
-    BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
-    DEMONS = []  # User id of support users
-    TIGERS = []  # User id of tiger users
-    WOLVES = []  # User id of whitelist users
+    BL_CHATS = [6032136577]  # List of groups that you want blacklisted.
+    DRAGONS = [6032136577]  # User id of sudo users
+    DEV_USERS = [6032136577]  # User id of dev users
+    DEMONS = [6032136577]  # User id of support users
+    TIGERS = [6032136577]  # User id of tiger users
+    WOLVES = [6032136577]  # User id of whitelist users
 
     ALLOW_CHATS = True
     ALLOW_EXCL = True
