aa2a1dd5dfa214052b5c6f98664850fec16d4b39
Drstrangee83 <122635577+Drstrangee83@users.noreply.github.com>
2023-08-27 07:53:55 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop SANJANAXRobot
-net start SANJANAXRobot
+net stop mukeshRobot
+net start MukeshRobot
